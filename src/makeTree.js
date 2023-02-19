3ee6baa50d31b049c529a7c2bc7e64404174f7a8
Syusina Daria <syusinad@mail.ru>
2023-02-19 20:34:53 +0500

    fixed mistakesэ

--- src/makeTree.js
+++ src/makeTree.js
@@ -1,11 +1,9 @@
 import _ from 'lodash';
 
 const makeTree = (file1, file2) => {
-  const key1 = Object.keys(file1);
-  const key2 = Object.keys(file2);
-  const keys = _.sortBy(_.union(key1, key2));
+  const keys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
 
-  const result = keys.map((key) => {
+  return result = keys.map((key) => {
     if (_.isObject(file1[key]) && _.isObject(file2[key])) {
       return { type: 'nested', name: key, children: makeTree(file1[key], file2[key]) };
     }
@@ -16,10 +14,11 @@ const makeTree = (file1, file2) => {
       return { type: 'removed', name: key, value: file1[key] };
     }
     if (!_.isEqual(file1[key], file2[key])) {
-      return { type: 'update', name: key, value1: file1[key], value2: file2[key] };
+      return { 
+        type: 'update', name: key, value1: file1[key], value2: file2[key] 
+      };
     }
     return { type: 'unchange', name: key, value: file1[key] };
   });
-  return result;
 };
 export default makeTree;
